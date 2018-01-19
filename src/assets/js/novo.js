71060d8889f79c567eb22d384cb124e091cf6b09
Geovane Feitosa Cavalcante <geovanefeitosacavalcante@gmail.com>
2018-01-18 22:03:31 -0300

    novo

---
+++ src/assets/js/novo.js
@@ -0,0 +1,8 @@
+
+     $('.button-collapse').sideNav({
+      menuWidth: 270,
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) {}
+    }
+  );
